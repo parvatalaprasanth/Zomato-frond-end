ad7d25e2b0527f379180fe3ec524353c30e9fbbf
parvatalaprasanth <49365892+parvatalaprasanth@users.noreply.github.com>
2021-05-13 01:13:59 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: 'https://zomato-backend-spark.herokuapp.com/' 
+  baseURL: 'enter your api endpoint/' 
     
 });
 
